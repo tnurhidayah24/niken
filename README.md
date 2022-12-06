4b940c008f144dea34dbaee03bc8ff0ffeb3d3de
Taufik nurhidayah <tnurhidayah24@gmail.com>
2022-12-06 15:17:41 +0700

    install tailwind with npm

---
+++ README.md
@@ -0,0 +1,1 @@
+Name author : Taufik Nurhidayah
--- public/index.php
+++ public/index.php
@@ -1,12 +1,11 @@
 <!DOCTYPE html>
 <html lang="en" dir="ltr">
-  <head>
-    <meta charset="utf-8">
-    <title>Niken</title>
-  </head>
-  <body>
-    <h1 class="">
-      Hello world!
-    </h1>
-  </body>
+	<head>
+		<meta charset="utf-8">
+		<title>Niken</title>
+		<link rel="stylesheet" href="assets/css/style.css">
+	</head>
+	<body>
+		<h1>Hello World</h1>
+	</body>
 </html>
