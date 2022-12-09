e1d8d25133a6fbf784fa83ba22f7de30282df6fd
Taufik nurhidayah <tnurhidayah24@gmail.com>
2022-12-10 00:06:22 +0700

    header

--- public/inc/Navbar.php
+++ public/inc/Navbar.php
@@ -1,49 +1,29 @@
-<div class="navbar bg-base-100">
-    <div class="navbar-start">
-        <div class="dropdown">
-        <label tabindex="0" class="btn btn-ghost lg:hidden">
-            <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
-        </label>
-        <ul tabindex="0" class="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
-            <li><a>Shoes</a></li>
-            <li><a>Jackets</a></li>
-            <li><a>T-Shirts</a></li>
-            <li><a>Sports Bras</a></li>
-            <li tabindex="0">
-            <a class="justify-between">
-                Gender
-                <svg class="fill-current" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M8.59,16.58L13.17,12L8.59,7.41L10,6L16,12L10,18L8.59,16.58Z"/></svg>
-            </a>
-            <ul class="p-2">
-                <li><a>Men</a></li>
-                <li><a>Women</a></li>
-                <li><a>Kids</a></li>
+<div class="container mx-auto max-w-7xl">
+    <div class="navbar bg-base-100">
+        <div class="navbar-start">
+            <div class="dropdown">
+            <label tabindex="0" class="btn btn-ghost lg:hidden">
+                <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
+            </label>
+            <ul tabindex="0" class="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
+                <li><a>Shoes</a></li>
+                <li><a>Jackets</a></li>
+                <li><a>T-Shirts</a></li>
+                <li><a>Sports Bras</a></li>
             </ul>
-            </li>
-        </ul>
+            </div>
+            <a class="btn btn-ghost normal-case text-xl">Niken</a>
         </div>
-        <a class="btn btn-ghost normal-case text-xl">Niken</a>
-    </div>
-    <div class="navbar-center hidden lg:flex">
-        <ul class="menu menu-horizontal p-0">
-        <li><a>Shoes</a></li>
-        <li><a>Jackets</a></li>
-        <li><a>T-Shirts</a></li>
-        <li><a>Sport Bras</a></li>
-        <li tabindex="0">
-            <a>
-            Gender
-            <svg class="fill-current" xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24"><path d="M7.41,8.58L12,13.17L16.59,8.58L18,10L12,16L6,10L7.41,8.58Z"/></svg>
-            </a>
-            <ul class="p-2">
-            <li><a>Men</a></li>
-            <li><a>Women</a></li>
-            <li><a>Kids</a></li>
+        <div class="navbar-center hidden lg:flex">
+            <ul class="menu menu-horizontal p-0">
+                <li><a>Shoes</a></li>
+                <li><a>Jackets</a></li>
+                <li><a>T-Shirts</a></li>
+                <li><a>Sport Bras</a></li>
             </ul>
-        </li>
-        </ul>
-    </div>
-    <div class="navbar-end">
-        <a class="btn">Get started</a>
+        </div>
+        <div class="navbar-end">
+            <a class="btn">Get started</a>
+        </div>
     </div>
 </div>
--- public/index.php
+++ public/index.php
@@ -6,8 +6,32 @@
 		<link rel="stylesheet" href="assets/css/style.css">
 	</head>
 	<body>
-		<div class="container mx-auto max-w-7xl">
+		<header>
 			<?php include('inc/Navbar.php') ?>
-		</div>
+		</header>
+		<main>
+			<section class=" h-fit bg-slate-700">
+				<div class="py-16 sm:px-2 lg:py-20 lg:px-0 max-w-7xl mx-auto">
+					<div class="grid  grid-cols-1 items-center gap-y-16 gap-x-8 px-4 lg:max-w-8xl lg:grid-cols-2 lg:px-8 xl:gap-x-16 xl:px-12">
+						<div class="relative z-10 md:text-center lg:text-left">
+							<div class="relative">
+								<p class="inline bg-gradient-to-r from-indigo-200 via-sky-400 to-indigo-200 bg-clip-text font-display text-5xl tracking-tight text-transparent">Be confident with your retro style.</p>
+								<p class="mt-3 text-2xl tracking-tight text-slate-400">The Air Jordan 7 Retro SE is ready to take on the day. Retro style and make people see our good looks.</p>
+								<div class="mt-8 flex gap-4 md:justify-center lg:justify-start">
+									<a class="rounded-full bg-sky-300 py-2 px-4 text-sm font-semibold text-slate-900 hover:bg-sky-200 focus:outline-none focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-sky-300/50 active:bg-sky-500" href="">Get started</a>
+									<a class="rounded-full bg-slate-800 py-2 px-4 text-sm font-medium text-white hover:bg-slate-700 focus:outline-none focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-white/50 active:text-slate-400" href="https://www.nike.com/id/t/air-jordan-7-retro-se-shoes-xjGbvR/DZ4729-200">View on Nike</a>
+								</div>
+							</div>
+						</div>	
+						<div class="xl:pl-10 grayscale hover:grayscale-0">
+							<img src="assets/img/niken-1.png" alt="" srcset="">
+						</div>
+					</div>
+				</div>
+			</section>
+			<footer>
+
+			</footer>
+		</main>
 	</body>
 </html>
