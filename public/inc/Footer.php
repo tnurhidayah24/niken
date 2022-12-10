4fc0efaa2c86352a30461c1466656e93b7be0ed6
Taufik nurhidayah <tnurhidayah24@gmail.com>
2022-12-10 15:58:32 +0700

    footer

---
+++ public/inc/Footer.php
@@ -0,0 +1,24 @@
+<div>
+    <svg aria-hidden="true" class="pre-logo-svg" focusable="false" viewBox="0 0 20 20" role="img" width="60px" height="60px" fill="none"><path fill="currentColor" fill-rule="evenodd" d="M21 8.719L7.836 14.303C6.74 14.768 5.818 15 5.075 15c-.836 0-1.445-.295-1.819-.884-.485-.76-.273-1.982.559-3.272.494-.754 1.122-1.446 1.734-2.108-.144.234-1.415 2.349-.025 3.345.275.2.666.298 1.147.298.386 0 .829-.063 1.316-.19L21 8.719z" clip-rule="evenodd"></path></svg>
+    <p>Niken Industries Ltd.<br/>Copyright © <span x-text="new Date().getFullYear()"></span></p>
+</div> 
+<div>
+    <span class="footer-title">Product</span> 
+    <a class="link link-hover">Shoes</a> 
+    <a class="link link-hover">Jackets</a> 
+    <a class="link link-hover">T-Shirts</a> 
+    <a class="link link-hover">Sports Bras</a>
+</div> 
+<div>
+    <span class="footer-title">Company</span> 
+    <a class="link link-hover">About us</a> 
+    <a class="link link-hover">Contact</a> 
+    <a class="link link-hover">News</a> 
+    <a class="link link-hover">Investors</a>
+</div> 
+<div>
+    <span class="footer-title">Legal</span> 
+    <a class="link link-hover">Terms of use</a> 
+    <a class="link link-hover">Privacy policy</a> 
+    <a class="link link-hover">Cookie policy</a>
+</div>
--- public/index.php
+++ public/index.php
@@ -21,7 +21,7 @@
 								<p class="mt-3 text-2xl tracking-tight text-slate-400">The Air Jordan 7 Retro SE is ready to take on the day. Retro style and make people see our good looks.</p>
 								<div class="mt-8 flex gap-4 md:justify-center lg:justify-start">
 									<a class="rounded-full bg-sky-300 py-2 px-4 text-sm font-semibold text-slate-900 hover:bg-sky-200 focus:outline-none focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-sky-300/50 active:bg-sky-500" href="">Get started</a>
-									<a class="rounded-full bg-slate-800 py-2 px-4 text-sm font-medium text-white hover:bg-slate-700 focus:outline-none focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-white/50 active:text-slate-400" href="https://www.nike.com/id/t/air-jordan-7-retro-se-shoes-xjGbvR/DZ4729-200">View on Nike</a>
+									<a class="rounded-full bg-slate-800 py-2 px-4 text-sm font-medium text-white hover:bg-slate-600 focus:outline-none focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-white/50 active:text-slate-400" href="https://www.nike.com/id/t/air-jordan-7-retro-se-shoes-xjGbvR/DZ4729-200">View on Nike</a>
 								</div>
 							</div>
 						</div>	
@@ -32,10 +32,8 @@
 				</div>
 			</section>
 		</main>
-		<footer>
-			<div class=" container text-center">
-				Copyright © <span x-text="new Date().getFullYear()"></span>
-			</div>
+		<footer class="footer p-10 text-base-content max-w-7xl mx-auto">
+			<?php include('inc/Footer.php')?>
 		</footer>
 	</body>
 </html>
